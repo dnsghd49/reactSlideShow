81560dcbb36ccb301fdb2a03872972f1b3f6d0de
Aaron Lu <aaronnoh0409@gmail.com>
2022-01-29 10:50:15 -0500

    add a art gall with buttons that pretend to be a slide button

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import { useState, useEffect } from 'react'
+import Gallery from './components/Gallery';
+import ButtonBar from './components/ButtonBar';
+
+function App() {
+  let [artId, setArtId] = useState(12720)
+  let [data, setData] = useState({})
+
+  useEffect(() => {
+    document.title = 'Welcome to Artworld'
+    fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${artId}`)
+      .then(response => response.json())
+      .then(resData => setData(resData))
+  }, [artId])
+
+  const handleIterate = (e) => {
+    setArtId(artId + Number(e.target.value))
+  }
+  return (
+    <div className='App'>
+      <Gallery objectImg={data.primaryImage} artist={data.artistDisplayName} title={data.title} />
+      <ButtonBar handleIterate={handleIterate} />
+    </div>
+  )
+}
+
+export default App;
